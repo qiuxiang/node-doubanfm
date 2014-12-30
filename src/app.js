a344e1f182fcf92f3a3fd69fc87b179fb8dfec36
Qiu Xiang <xiang.qiu@qq.com>
2014-12-30 23:28:03 +0800

    实现专辑封面的显示

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 var net = require('net')
+  , port = require(process.env.HOME + '/.pydoubanfm/setting.json').port
 
 angular.module('doubanfm', []).controller('Doubanfm', ['$scope', function ($scope) {
   var client = new net.Socket(),
       handler = {
         song: function (song) {
           console.log(song)
+          $scope.album_cover = song.picture
+          $scope.$apply()
         },
         channels: function (channels) {
           console.log(channels)
         }
       }
 
-  client.connect(1234, 'localhost', function () {
+  client.connect(port, 'localhost', function () {
     client.write('song\nchannels')
   })
 
@@ -19,13 +22,17 @@ angular.module('doubanfm', []).controller('Doubanfm', ['$scope', function ($scop
     String(data).split('\n').forEach(function (row) {
       if (row) {
         row = JSON.parse(row)
-        if (row.length == 1) {
-          handler[row[0]]()
+        var func = handler[row[0]]
+        if (func) {
+          if (row.length == 1) {
+            func()
+          } else {
+            func(row[1])
+          }
         } else {
-          handler[row[0]](row[1])
+          console.log('handler \'' + row[0] + '\' not exist')
         }
       }
     })
   })
-
 }])
